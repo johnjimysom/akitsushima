d6dcf940af8f00d500964a08a40c69812f7c7cf5
John-Jimi Som <johnjimy.som@student.csulb.edu>
2021-06-25 11:12:40 -0700

    added akitsushima files

---
+++ akitsushima.py
@@ -0,0 +1,12 @@
+#while loop and password studies
+
+#while True:
+#    print('who are you?')
+#    name = input()
+#    if name != 'kama':
+#        continue
+#    print('hello, Kama. What is the password? (It is a noble phantasm.)')
+#    password = input()
+#    if password == 'samsara kama':
+#        break
+#print('ACCESS GRANTED')
